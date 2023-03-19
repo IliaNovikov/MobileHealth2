af0a24475614fbcd4d6a295de589bd397eff247d
CUMrade228 <ilia.pp.novikov@gmail.com>
2023-03-19 22:16:29 +0500

    Реализовал клик на элемент главного RecyclerView, смену текста и фона в toolbar, добавил класс PFCFragment

--- app/src/main/java/com/example/mobilehealth/MainActivity.java
+++ app/src/main/java/com/example/mobilehealth/MainActivity.java
@@ -1,24 +1,49 @@
 package com.example.mobilehealth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import services.FragmentService;
 
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout frameLayout;
+    private TextView tvFragmentName;
+    private Toolbar toolbarMain;
+    private String fragmentName;
+    private int fragmentColor;
+
+    public void setFragmentColor(int fragmentColor) {
+        this.fragmentColor = fragmentColor;
+        toolbarMain.setBackgroundColor(fragmentColor);
+    }
+
+    public void setFragmentName(String fragmentName) {
+        this.fragmentName = fragmentName;
+        tvFragmentName.setText(fragmentName);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         init();
+        FragmentService.currentFragmentTitle = getString(R.string.main_page);
+        FragmentService.currentFragmentColor = getColor(R.color.black);
         FragmentService.setFragment(this, new MainFragment(), R.id.main_frameLayout);
     }
+
     private void init(){
         frameLayout = findViewById(R.id.main_frameLayout);
+        tvFragmentName = findViewById(R.id.tvFragmentName);
+        toolbarMain = findViewById(R.id.toolbar);
     }
 }
